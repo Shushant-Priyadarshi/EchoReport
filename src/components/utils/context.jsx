8a2244432c7fed54f16b623380f0f51334feb944
Shushant-Priyadarshi <profshushant@gmail.com>
2024-06-04 22:34:10 +0530

    vecel error solved

--- src/components/utils/context.jsx
+++ src/components/utils/context.jsx
@@ -1,7 +1,7 @@
 import { createContext, useContext, useEffect, useReducer } from "react";
 import reducer from "./Reducer";
 
-let API = "http://hn.algolia.com/api/v1/search?";
+let API = "https://hn.algolia.com/api/v1/search?";
 const initialState = {
   isLoading: true,
   query: "",
